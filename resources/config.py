4d4514abe5de33ef0c0713669c478acaa16a3cf9
Blair Bowen <blairbowen35@gmail.com>
2025-02-03 18:41:12 -0500

    got it to work with real table

---
+++ resources/config.py
@@ -0,0 +1,9 @@
+
+sql_server = 'knowball.database.windows.net'
+sql_database ='knowball-sql'
+sql_username = 'kbserver'
+sql_password = 'ciminibb$bowenbv$king3ss$'
+
+nba_table = 'nba_player'
+nfl_table = 'nfl_player'
+soccer_table = 'epl_player'
