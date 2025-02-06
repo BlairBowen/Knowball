5de3bd8f096854ceb93b35f0a94f6bb207cb75cf
Ben Cimini <ciminibb@mail.uc.edu>
2025-02-06 14:28:19 -0500

    #43 title card component created with custom font

---
+++ frontend/src/components/TitleCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TitleCard = () => {
+  return (
+    <div className="bg-gradient-to-r from-pink-300 to-teal-300 p-8 rounded-lg shadow-lg text-center">
+      <h1 className="text-4xl md:text-6xl font-tourney text-white tracking-wide">
+        Knowball
+      </h1>
+    </div>
+  );
+};
+
+export default TitleCard;
