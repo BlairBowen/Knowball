b61bdc1fd80968939c207bfa0773fb8cd8da0052
Ben Cimini <ciminibb@mail.uc.edu>
2025-02-06 14:58:38 -0500

    #43 styling changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import TitleCard from './components/TitleCard';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-300 to-green-300">
       <TitleCard />
     </div>
   );
--- frontend/src/components/TitleCard.jsx
+++ frontend/src/components/TitleCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TitleCard = () => {
   return (
-    <div className="bg-gradient-to-r from-pink-300 to-teal-300 p-8 rounded-lg shadow-lg text-center">
+    <div className="p-8 text-center">
       <h1 className="text-4xl md:text-6xl font-tourney text-white tracking-wide">
         Knowball
       </h1>
