fdc599ce60bfea798dcf0425d290dfee98b72e9e
Ben Cimini <ciminibb@mail.uc.edu>
2025-02-07 12:14:48 -0500

    #43 added icons to title card

--- frontend/src/components/TitleCard.jsx
+++ frontend/src/components/TitleCard.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import basketball from "../assets/pixel bball.png";
+import soccerball from "../assets/pixel sball.png";
+import football from "../assets/pixel fball.png";
 
 const TitleCard = () => {
   return (
-    <div className="p-8 pb-0">
+    <div className="p-8 pb-0 flex justify-between items-center">
       <h1 className="font-tourney text-black tracking-wide">
         Knowball
       </h1>
+      <div className="flex space-x-2">
+        {[basketball, soccerball, football].map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt="Pixel sport icon"
+            className="w-16 h-16 cursor-pointer"
+          />
+        ))}
+      </div>
     </div>
   );
 };
