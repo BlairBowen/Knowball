e4c6a0b478f0f2fc305ad3a4a17a15b9c77c7217
Ben Cimini <ciminibb@mail.uc.edu>
2025-02-13 20:53:02 -0500

    #45 sports separated and home screen added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,21 @@
-import React, { useState } from 'react';
-import TitleCard from './components/TitleCard';
-import QuestionBox from './components/QuestionBox';
-import InputBox from './components/InputBox';
-import AnswersList from './components/AnswersList';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import NBA from "./screens/nba";
+import EPL from "./screens/EPL";
+import NFL from "./screens/NFL";
 
 function App() {
-  // State to hold the list of answers
-  const [answers, setAnswers] = useState([]);
-
   return (
-    <div className='min-h-screen w-screen bg-gradient-to-r from-blue-300 to-green-300'>
-      <div className='max-w-400 flex flex-col'>
-        <TitleCard />
-        <QuestionBox />
-
-        {/* Columnar alignment for input and answer boxes. */}
-        <div className='flex'>
-          <div className='w-1/2'>
-            {/* Pass setAnswers to InputBox to handle updates */}
-            <InputBox setAnswers={setAnswers} />
-          </div>
-          <div className='w-1/2'>
-            {/* Pass answers to AnswersList to display them */}
-            <AnswersList answers={answers} />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nba" element={<NBA />} />
+        <Route path="/epl" element={<EPL />} />
+        <Route path="/nfl" element={<NFL />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
